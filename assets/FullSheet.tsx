03877882a75d43f1238b9ce2aa80e256ac4367b9
Matooki <dpazos@ucsc.edu>
2020-06-05 17:48:07 -0700

    commit: new levels

---
+++ assets/FullSheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="FullSheet" tilewidth="16" tileheight="16" tilecount="29172" columns="187">
+ <image source="FullSheet.png" width="3000" height="2500"/>
+</tileset>
